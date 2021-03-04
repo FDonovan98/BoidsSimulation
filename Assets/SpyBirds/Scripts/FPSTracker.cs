1ae7a62746bae47250b9c7ee06577d87580fef97
hd241610 <hd241610@falmouth.ac.uk>
2021-03-04 14:58:38 +0000

    Add an FPS tracker

---
+++ Assets/SpyBirds/Scripts/FPSTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSTracker : MonoBehaviour
+{
+    Text text;
+    int frameCount = 0;
+    float deltaTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += Time.deltaTime;
+        frameCount++;
+        if (frameCount > 10)
+        {
+            text.text = (20.0f / deltaTime).ToString();
+            frameCount = 0;
+            deltaTime = 0.0f;
+        }
+    }
+}
